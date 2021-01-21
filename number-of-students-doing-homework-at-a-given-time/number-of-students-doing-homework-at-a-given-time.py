62bff36c7a01a8c911235305b09f79a7a4ab2b78
Manasa Balaji <manasbalaji4857@gmail.com>
2021-01-20 20:45:43 -0600

    Time: 36 ms (75.48%), Space: 14.2 MB (54.76%) - LeetHub

---
+++ number-of-students-doing-homework-at-a-given-time/number-of-students-doing-homework-at-a-given-time.py
@@ -0,0 +1,8 @@
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        c=0
+        for i in range(0,len(startTime)):
+            if(queryTime>=startTime[i] and queryTime<=endTime[i]):
+                c+=1
+        return c
+            
